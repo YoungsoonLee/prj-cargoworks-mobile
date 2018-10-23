ff109ae890a2d2ce97c09486c56dd21750300388
James Kang <phantombm@smartlink.io>
2018-10-23 18:32:55 +0900

    save

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var coffee = require('gulp-coffee');
 var exec = require('child_process').exec;
 var replace = require('gulp-replace');
 var watch = require('gulp-watch');
+var cached = require('gulp-cached');
 
 var srcs = {
     coffeeToJs: [
@@ -20,6 +21,7 @@ gulp.task('clean', function() {
 gulp.task('coffee to js', function() {
   var coffeeToJs = function () {
     gulp.src(srcs.coffeeToJs)
+      .pipe(cached('coffee to js'))
       .pipe(replace('.coffee', '.js'))
       .pipe(replace('require \'../', 'require \'../../'))
       .pipe(coffee({ bare: true }))
